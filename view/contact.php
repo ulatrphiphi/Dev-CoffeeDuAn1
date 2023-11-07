94ab8e6da32870b8a2a88f11e1ea44ca22abc17b
haohtpc05526 <trunghaopc23579@gmail.com>
2023-11-07 14:28:25 +0700

    [HTML] - Contact page việt hoá giao diện 2

--- view/contact.php
+++ view/contact.php
@@ -1,74 +1,71 @@
+<section class="home-slider owl-carousel">
 
+	<div class="slider-item" style="background-image: url(/view/images/bg_3.jpg);" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row slider-text justify-content-center align-items-center">
 
-    <section class="home-slider owl-carousel">
+				<div class="col-md-7 col-sm-12 text-center ftco-animate">
+					<h1 class="mb-3 mt-5 bread">Liên Hệ</h1>
+					<p class="breadcrumbs"><span class="mr-2"><a href="index.php"></a></span>Trang chủ<span>Liên hệ</span></p>
+				</div>
 
-      <div class="slider-item" style="background-image: url(images/bg_3.jpg);" data-stellar-background-ratio="0.5">
-      	<div class="overlay"></div>
-        <div class="container">
-          <div class="row slider-text justify-content-center align-items-center">
+			</div>
+		</div>
+	</div>
+</section>
 
-            <div class="col-md-7 col-sm-12 text-center ftco-animate">
-            	<h1 class="mb-3 mt-5 bread">Contact Us</h1>
-	            <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Contact</span></p>
-            </div>
+<section class="ftco-section contact-section">
+	<div class="container mt-5">
+		<div class="row block-9">
+			<div class="col-md-4 contact-info ftco-animate">
+				<div class="row">
+					<div class="col-md-12 mb-4">
+						<h2 class="h4">Thông Tin Liên Hệ</h2>
+					</div>
 
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <section class="ftco-section contact-section">
-      <div class="container mt-5">
-        <div class="row block-9">
-					<div class="col-md-4 contact-info ftco-animate">
-						<div class="row">
-							<div class="col-md-12 mb-4">
-	              <h2 class="h4">Contact Information</h2>
-	            </div>
-	            <div class="col-md-12 mb-3">
-	              <p><span>Address:</span> 198 West 21th Street, Suite 721 New York NY 10016</p>
-	            </div>
-	            <div class="col-md-12 mb-3">
-	              <p><span>Phone:</span> <a href="tel://1234567920">+ 1235 2355 98</a></p>
-	            </div>
-	            <div class="col-md-12 mb-3">
-	              <p><span>Email:</span> <a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
-	            </div>
-	            <div class="col-md-12 mb-3">
-	              <p><span>Website:</span> <a href="#">yoursite.com</a></p>
-	            </div>
+					<div class="col-md-12 mb-3">
+						<p><span>Địa chỉ:</span> Đ. Số 22, Thường Thạnh, Cái Răng, Cần Thơ</p>
+					</div>
+					<div class="col-md-12 mb-3">
+						<p><span>Số điện thoại:</span> <a href="tel://+84 935 631 822">+84 935 631 822</a></p>
+					</div>
+					<div class="col-md-12 mb-3">
+						<p><span>Email:</span> <a href="mailto:3some@gmail.com">3some@gmail.com</a></p>
+					</div>
+					<div class="col-md-12 mb-3">
+						<p><span>Website:</span> <a href="#">devcoffee.vn</a></p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-1"></div>
+			<div class="col-md-6 ftco-animate">
+				<form action="#" class="contact-form">
+					<div class="row">
+						<div class="col-md-6">
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="Họ và Tên">
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="Email">
+							</div>
 						</div>
 					</div>
-					<div class="col-md-1"></div>
-          <div class="col-md-6 ftco-animate">
-            <form action="#" class="contact-form">
-            	<div class="row">
-            		<div class="col-md-6">
-	                <div class="form-group">
-	                  <input type="text" class="form-control" placeholder="Your Name">
-	                </div>
-                </div>
-                <div class="col-md-6">
-	                <div class="form-group">
-	                  <input type="text" class="form-control" placeholder="Your Email">
-	                </div>
-	                </div>
-              </div>
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Subject">
-              </div>
-              <div class="form-group">
-                <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
-              </div>
-              <div class="form-group">
-                <input type="submit" value="Send Message" class="btn btn-primary py-3 px-5">
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </section>
+					<div class="form-group">
+						<input type="text" class="form-control" placeholder="Tiêu Đề">
+					</div>
+					<div class="form-group">
+						<textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Ghi Chú"></textarea>
+					</div>
+					<div class="form-group">
+						<input type="submit" value="Gửi Tin Nhắn" class="btn btn-primary py-3 px-5">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</section>
 
-    <div id="map"></div>
-
-   
+<div id="map"></div>
