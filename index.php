6b37f723cec79a6b31aa98839352b0342cdeb283
sangtqpc05237 <tranquocsangstg2004@gmail.com>
2023-11-21 22:29:57 +0700

    [PHP] - Tạo case login và signup

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 session_start();
+include "model/pdo.php";
+// include "model/products.php";
+include "model/users.php";
+include "model/categories.php";
+// include "model/cart.php";
+// include "global.php";
 
 
 if ((isset($_GET['act'])) && ($_GET['act'] != "")) {
@@ -31,9 +37,33 @@ if ((isset($_GET['act'])) && ($_GET['act'] != "")) {
             include 'view/footer.php';
             break;
         case 'login':
-            include 'view/login.php';
+            if(isset($_POST['login'])&&($_POST['login'])){
+                $user=$_POST['user'];
+                $pass=$_POST['pass'];
+                $checkuser=checkuser($user, $pass);
+                if(is_array($checkuser)){
+                    $_SESSION['user']=$checkuser;
+                    header('Location: index.php?act=home');
+                }else{
+                    
+                    $thongbao="Tài khoản không tồn tại. Vui lòng kiểm tra hoặc đăng ký lại";
+                }
+            }else{  
+                include "view/login.php";
+            }
             break;
         case 'signup':
+                if(isset($_POST['signup'])&&($_POST['signup'])){
+                    $email=$_POST['email'];
+                    $user=$_POST['username'];
+                    $pass=$_POST['pass'];
+                    // $cpass=$_POST['cpass'];
+                    // $pass_hash = password_hash($pass, PASSWORD_DEFAULT);
+                    insert_user($email, $user, $pass);
+                    // echo '<script>alert("Đăng ký thành công, mời đăng nhập")</script>';
+                    header('LOCATION: index.php?act=login');
+    
+                }
             include 'view/signup.php';
             break;
         default:
