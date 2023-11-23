d0bea42788d5a88dc27468fe13b32325329b276e
Phi <gede123ct@gmail.com>
2023-11-23 16:50:00 +0700

    [PHP] - Model Categories

--- model/categories.php
+++ model/categories.php
@@ -1,17 +1,17 @@
 <?php
 function insert_categories($name)
 {
-    $sql = "insert into categories(name) values('$name ')";
+    $sql = "insert into categories(name) values('$name')";
     pdo_execute($sql);
 }
 function delete_categories($id)
 {
-    $sql = "delete from categories where id =" . $id;
+    $sql = "update categories set status = 0 where id =" . $id;
     pdo_execute($sql);
 }
 function load_all_categories()
 {
-    $sql = "select * from categories order by id desc";
+    $sql = "select * from categories where status = 1 order by id desc";
     $list_categories = pdo_query($sql);
     return $list_categories;
 }
@@ -19,8 +19,8 @@ function load_all_categories()
 function load_one_categories($id)
 {
     $sql = "select * from categories where id = " . $id;
-    $cate = pdo_query_one($sql);
-    return $cate;
+    $dm = pdo_query_one($sql);
+    return $dm;
 }
 function update_categories($id,$name)
 {
