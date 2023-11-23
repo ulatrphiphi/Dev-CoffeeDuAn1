a84ebf28c022275b9b34f100ae108ac09d5f5e98
Phi <gede123ct@gmail.com>
2023-11-23 16:50:35 +0700

    [PHP] - Danh sách danh mục

---
+++ admin/modules/categories/list.php
@@ -0,0 +1,58 @@
+<div class="card-footer">
+  <a href="index.php?act=addcate"><button type="submit" class="btn btn-primary" style="float:right;">Thêm Danh Mục</button></a>
+</div>
+
+<div class="row">
+  <div class="col-12">
+    <div class="card">
+      <div class="card-header">
+        <h3 class="card-title">Danh mục</h3>
+        <div class="card-tools">
+          <div class="input-group input-group-sm" style="width: 150px;">
+            <input type="text" name="table_search" class="form-control float-right" placeholder="Tìm Kiếm">
+
+            <div class="input-group-append">
+              <button type="submit" class="btn btn-default">
+                <i class="fas fa-search"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /.card-header -->
+      <div class="card-body table-responsive p-0" style="height: 300px;">
+
+        <table class="table table-head-fixed text-nowrap table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Tên Danh mục</th>
+              <th>Thao Tác</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            foreach ($listcategories as $category) {
+              extract($category);
+              $editcate = "index.php?act=editcate&id=" . $id;
+              $deletecate = "index.php?act=deletecate&id=" . $id;
+              echo '     <tr>    
+                    <td>' . $id . '</td>
+                    <td>' . $name . '</td>
+                    <td>
+                      <a href="'.$editcate.'" class="btn btn-primary">Sửa</a>
+                      <a href="'.$deletecate.'" class="btn btn-danger">Xóa</a>
+                    </td>
+                  </tr>
+                    ';
+            }
+            ?>
+          </tbody>
+        </table>
+
+        <div class="card-body">
+        </div>
+
+      </div>
+      <!-- /.card-body -->
+    </div>
