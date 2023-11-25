5423fb6e2c4be62599c1728e4de03a20c6e111ab
Phi <gede123ct@gmail.com>
2023-11-25 18:27:47 +0700

    [PHP] - Sửa Cập nhật sản phẩm

--- admin/modules/products/edit.php
+++ admin/modules/products/edit.php
@@ -6,60 +6,58 @@ if (is_array($products)) {
     extract($products);
     // Các biến như $id, $name, $price, $img, $detail đã được khai báo
 }
-$hinhpath = "../upload/" . $img;
-if (is_file($hinhpath)) {
-    $hinh = "<img src='" .  $hinhpath . "' height='80'";
+$img_path = "./uploads/" . $img;
+if (is_file($img_path)) {
+    $img = "<img src='" .  $img_path . "' height='80'";
 } else {
-    $hinh = "no photo";
+    $img = "no photo";
 }
 ?>
-
-
+<div class="card-footer"><a href="index.php?act=listpro"><button type="submit" class="btn btn-primary" style="float:left;">Danh sách sản phẩm</button></a></div>
 <div class="card card-primary">
     <div class="card-header">
-        <h3 class="card-title"> Sửa sản phẩm</h3>
+        <h3 class="card-title"> Thêm sản phẩm</h3>
     </div>
+    <!-- /.card-header -->
+    <!-- form start -->
+    <form action="index.php?act=addpro" method="POST" enctype="multipart/form-data">
+        <div class="card-body">
 
 
-    <form action="index.php?act=editpro" method="POST" enctype="multipart/form-data">
-            <div class="cart body">
-                <br>
-                <select name="iddm" id="">
-                    <option value="0" selected>Tất cả</option>
-                    <?php foreach ($list_categories as $categories) {
-                        extract($categories);
-                        if ($iddm == $id) $s = "selected";
-                        else $s = "";
-                        echo '<option value="' . $id . '" ' . $s . '>' . $name . '</option>';
-                    } ?>
+            <div class="form-group">
+               <label>Danh mục</label>
+               <select name="categories_id" class="form-control" id="">
+                            <?php foreach ($list_categories as $categories) {
+                                extract($categories);
+                                echo'<option value="'.$id.'">'.$name.'</option>';
 
-                </select>
+                            }?>
+                            
+                        </select>
             </div>
             <div class="form-group">
-                Tên sản phẩm <br>
-                <input type="text" name="tensp" id="" value="<?= $products['name'] ?>">
+                <label for="name">Tên sản phẩm</label>
+                <input type="text" class="form-control" name="name" value="<?=$products['name']?>">
             </div>
             <div class="form-group">
-                Giá <br>
-                <input type="text" name="giasp" value="<?= $products['price'] ?>">
+                <label for="price">Giá sản phẩm</label>
+                <input type="text" class="form-control" name="price" value="<?=$products['price']?>">
+            </div>
+            <div class="">
+                <label for="img">Ảnh sản phẩm</label>
+                <input type="file" class="" name="img">
+                <?=$img?>
             </div>
             <div class="form-group">
-                Hình ảnh <br>
-                <input type="file" name="hinh">
-                <?= $hinh ?>
+                <label for="detail">Mô tả sản phẩm</label>
+                <textarea class="form-control" id="summernote" name="detail" ><?=$products['detail']?></textarea>
             </div>
-            <div class="form-group">
-                Mô tả <br>
-                <textarea class="form-control" id="summernote" name="mota"><?= $products['detail'] ?? '' ?></textarea>
-            </div>
-            <div class="form-group">
-                <input type="hidden" name="id" value="<?= $products['id'] ?>">
-                <input type="submit" name="capnhat" value="Cập nhật">
-                <input type="reset" value="Nhập lại">
-                <a href="index.php?act=listpro"><input type="button" value="Danh sách"></a>
+            <div class="card-footer">
+                <input type="submit" class="btn btn-primary" name="addnew" value="Thêm mới">
+                <input type="reset" class="btn btn-primary" value="Nhập lại">
             </div>
             <?php
             if (isset($thongbao) && ($thongbao != "")) echo $thongbao;
             ?>
-        </form>
+    </form>
 </div>
