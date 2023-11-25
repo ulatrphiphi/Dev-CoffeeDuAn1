bb1dd9db9d2b73f00a86b4146b1f58bb9e443fb9
Phi <gede123ct@gmail.com>
2023-11-25 18:27:34 +0700

    [PHP] - Sửa thêm sản phẩm

--- admin/modules/products/add.php
+++ admin/modules/products/add.php
@@ -1,3 +1,4 @@
+<div class="card-footer"><a href="index.php?act=listpro"><button type="submit" class="btn btn-primary" style="float:left;">Danh sách sản phẩm</button></a></div>
 <div class="card card-primary">
     <div class="card-header">
         <h3 class="card-title"> Thêm sản phẩm</h3>
@@ -8,49 +9,36 @@
         <div class="card-body">
 
 
-            <div class="form-group card-body">
-                Danh mục<br>
-                <!-- <select name="iddm">
-                    <?php
-                    foreach ($list_categories as $categories) {
-                        extract($categories);
-                        echo '<option value=' . $id . '>' . $name . '</option>';
-                    }
-                    ?>
-                </select> -->
-                <form action="/admin/index.php" class="form-inline" method="GET">
-                    <input type="hidden" name="pages" value="posts">
-                    <input type="hidden" name="action" value="list">
-                    <select name="categories" id="categories" class="form-control mr-3">
-                        <?php
-                        foreach ($list_categories as $categories) {
-                            extract($categories);
-                            echo '<option value=' . $id . '>' . $name . '</option>';
-                        }
-                        ?>
-                    </select>
-                </form>
+            <div class="form-group">
+               <label>Danh mục</label>
+               <select name="categories_id" id="">
+                            <?php foreach ($list_categories as $categories) {
+                                extract($categories);
+                                echo'<option value="'.$id.'">'.$name.'</option>';
+
+                            }?>
+                            
+                        </select>
             </div>
             <div class="form-group">
-                Tên sản phẩm <br>
-                <input type="text" name="tensp">
+                <label for="name">Tên sản phẩm</label>
+                <input type="text" class="form-control" name="name" placeholder="Nhập tên sản phẩm">
             </div>
             <div class="form-group">
-                Giá sản phẩm <br>
-                <input type="text" name="giasp">
+                <label for="price">Giá sản phẩm</label>
+                <input type="text" class="form-control" name="price" placeholder="Nhập giá sản phẩm">
             </div>
             <div class="form-group">
-                Ảnh <br>
-                <input type="file" name="hinh">
+                <label for="img">Ảnh sản phẩm</label>
+                <input type="file" class="form-control" name="img" placeholder="Nhập ảnh sản phẩm">
             </div>
             <div class="form-group">
-                Mô tả<br>
-                <textarea class="form-control" id="summernote" name="content"><?= $products['detail'] ?? '' ?></textarea>
+                <label for="detail">Mô tả sản phẩm</label>
+                <textarea class="form-control" id="summernote" name="detail"></textarea>
             </div>
-            <div class="form-group">
-                <input type="submit" name="themmoi" value="Thêm mới">
-                <input type="reset" value="Nhập lại">
-                <a href="index.php?act=listpro"> <input type="button" value="Danh sách"></a>
+            <div class="card-footer">
+                <input type="submit" class="btn btn-primary" name="addnew" value="Thêm mới">
+                <input type="reset" class="btn btn-primary" value="Nhập lại">
             </div>
             <?php
             if (isset($thongbao) && ($thongbao != "")) echo $thongbao;
