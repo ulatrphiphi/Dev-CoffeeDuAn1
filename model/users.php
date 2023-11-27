aeb4b832dfb2fecd25bf21f6c91f00e32beeefe6
Phi <gede123ct@gmail.com>
2023-11-27 14:45:50 +0700

    [PHP] - Sửa model user

--- model/users.php
+++ model/users.php
@@ -1,44 +1,54 @@
 <?php
 
-function loadall_user(){
-    $sql="select * from users where status = 1  order by id desc";
-    $listuser=pdo_query($sql);
+function loadall_user()
+{
+    $sql = "select * from users where status = 1  order by id desc";
+    $listuser = pdo_query($sql);
     return $listuser;
 }
 
-function insert_user($email, $user, $pass, $cpass ){
-    if($pass === $cpass){
-        $pass = password_hash($pass, PASSWORD_DEFAULT);
-        $sql="insert into users(email,user,pass) values('$email', '$user', '$pass')";
-        pdo_execute($sql);
-    }else{
+function insert_user($email, $user, $pass, $cpass)
+{
+    if ($pass === $cpass) {
+        $pass_hash = password_hash($pass, PASSWORD_DEFAULT);
+        $sql = "insert into users(email,user,pass) values(? , ?,  ?)";
+        pdo_execute($sql, $email, $user, $pass_hash);
+        header('LOCATION: index.php?act=login');
+    } else {
         echo '<script>alert("Mật khẩu nhập lại không đúng")</script>';
-        
     }
-    
 }
 
-function checkuser($user, $pass){
-    $sql="select * from users where user='".$user."' AND pass='".$pass."'";
-    $user=pdo_query_one($sql);
-    return $user;
+function checkuser($user)
+{
+    $sql = "select * from users where user = '" . $user . "'";
+    $selectUser = pdo_query_one($sql);
+    return $selectUser;
 }
-function checkemail($email){
-    $sql="select * from users where email='".$email."'";
-    $user=pdo_query_one($sql);
-    if($sql){
+function getUser()
+{
+    $user = $_SESSION['user_id'];
+    $sql = "select user from users WHERE id = $user";
+    $selectUser = pdo_query_one($sql);
+    return $selectUser;
+}
+function checkemail($email)
+{
+    $sql = "select * from users where email='" . $email . "'";
+    $user = pdo_query_one($sql);
+    if ($sql) {
         return $user;
-    }else{
+    } else {
         echo '<h4>Email không tồn tại</h4>';
     }
-    
 }
 
 
 
-function update_user($id, $user, $pass, $email, $address, $tel){
-    
-    $sql="update users set user='".$user."', pass='".$pass."', email='".$email."', address='".$address."', tel='".$tel."'   where id=".$id;
+function update_user($id, $user, $pass, $email, $address, $tel)
+{
+
+    $sql = "update users set user='" . $user . "', pass='" . $pass . "', email='" . $email . "', address='" . $address . "', tel='" . $tel . "'   where id=" . $id;
     pdo_execute($sql);
 }
 
@@ -47,7 +57,3 @@ function delete_user($id)
     $sql = "update users set status = 0 where id=" . $id;
     pdo_execute($sql);
 }
-
-
-
-?>
