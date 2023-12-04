80088cae05682bc8980e9f16160a3c2ed355cc4c
Phi <gede123ct@gmail.com>
2023-12-04 20:21:44 +0700

    [HTML] - Việt Hóa giao diện trang about

--- view/about.php
+++ view/about.php
@@ -6,8 +6,8 @@
 			<div class="row slider-text justify-content-center align-items-center">
 
 				<div class="col-md-7 col-sm-12 text-center ftco-animate">
-					<h1 class="mb-3 mt-5 bread">About Us</h1>
-					<p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>About</span></p>
+					<h1 class="mb-3 mt-5 bread">Về Chúng Tôi</h1>
+					<p class="breadcrumbs"><span class="mr-2"><a href="index.php">Home</a></span> <span>About</span></p>
 				</div>
 
 			</div>
@@ -16,185 +16,169 @@
 </section>
 
 <section class="ftco-about d-md-flex">
-	<div class="one-half img" style="background-image: url(/view/images/about.jpg);"></div>
-	<div class="one-half ftco-animate">
-		<div class="overlap">
-			<div class="heading-section ftco-animate ">
-				<span class="subheading">Discover</span>
-				<h2 class="mb-4">Our Story</h2>
-			</div>
-			<div>
-				<p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country. But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their.</p>
-			</div>
-		</div>
-	</div>
+  <div class="one-half img" style="background-image: url(/view/images/about.jpg)"></div>
+  <div class="one-half ftco-animate">
+    <div class="overlap">
+      <div class="heading-section ftco-animate">
+        <span class="subheading">Khám Phá</span><br>
+        <h2 class="mb-4">Câu Chuyện Của Chúng Tôi</h2>
+      </div>
+      <div>
+        <p>
+          Với mong muốn mang đến cho những người yêu công nghệ một không gian thư giãn và thoải mái, quán cà phê DEV coffee đã được thành lập vào năm 2023.
+
+          Nội thất của quán được thiết kế theo phong cách cổ điện, với tông màu chủ đạo là nâu và đen. Quán có đầy đủ các loại đồ uống cà phê, trà, bánh ngọt và đồ ăn nhẹ.
+
+          DEV coffee là địa điểm lý tưởng để học tập, làm việc hoặc đơn giản là thư giãn. Quán có wifi tốc độ cao, máy in và máy chiếu để phục vụ nhu cầu của khách hàng. Đội ngũ nhân viên của quán được đào tạo bài bản, luôn sẵn sàng phục vụ khách hàng một cách tận tình và chu đáo.
+
+        </p>
+      </div>
+    </div>
+  </div>
 </section>
 
-<section class="ftco-section img" id="ftco-testimony" style="background-image: url(images/bg_1.jpg);" data-stellar-background-ratio="0.5">
-	<div class="overlay"></div>
-	<div class="container">
-		<div class="row justify-content-center mb-5">
-			<div class="col-md-7 heading-section text-center ftco-animate">
-				<span class="subheading">Testimony</span>
-				<h2 class="mb-4">Customers Says</h2>
-				<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-			</div>
-		</div>
-	</div>
-	<div class="container-wrap">
-		<div class="row d-flex no-gutters">
-			<div class="col-lg align-self-sm-end">
-				<div class="testimony">
-					<blockquote>
-						<p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small.&rdquo;</p>
-					</blockquote>
-					<div class="author d-flex mt-4">
-						<div class="image mr-3 align-self-center">
-							<img src="images/person_1.jpg" alt="">
-						</div>
-						<div class="name align-self-center">Louise Kelly <span class="position">Illustrator Designer</span></div>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg align-self-sm-end">
-				<div class="testimony overlay">
-					<blockquote>
-						<p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.&rdquo;</p>
-					</blockquote>
-					<div class="author d-flex mt-4">
-						<div class="image mr-3 align-self-center">
-							<img src="images/person_2.jpg" alt="">
-						</div>
-						<div class="name align-self-center">Louise Kelly <span class="position">Illustrator Designer</span></div>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg align-self-sm-end">
-				<div class="testimony">
-					<blockquote>
-						<p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name. &rdquo;</p>
-					</blockquote>
-					<div class="author d-flex mt-4">
-						<div class="image mr-3 align-self-center">
-							<img src="images/person_3.jpg" alt="">
-						</div>
-						<div class="name align-self-center">Louise Kelly <span class="position">Illustrator Designer</span></div>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg align-self-sm-end">
-				<div class="testimony overlay">
-					<blockquote>
-						<p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however.&rdquo;</p>
-					</blockquote>
-					<div class="author d-flex mt-4">
-						<div class="image mr-3 align-self-center">
-							<img src="images/person_2.jpg" alt="">
-						</div>
-						<div class="name align-self-center">Louise Kelly <span class="position">Illustrator Designer</span></div>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg align-self-sm-end">
-				<div class="testimony">
-					<blockquote>
-						<p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name. &rdquo;</p>
-					</blockquote>
-					<div class="author d-flex mt-4">
-						<div class="image mr-3 align-self-center">
-							<img src="images/person_3.jpg" alt="">
-						</div>
-						<div class="name align-self-center">Louise Kelly <span class="position">Illustrator Designer</span></div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+<section class="ftco-section ftco-services">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 ftco-animate">
+        <div class="media d-block text-center block-6 services">
+          <div class="icon d-flex justify-content-center align-items-center mb-5">
+            <span class="flaticon-choices"></span>
+          </div>
+          <div class="media-body">
+            <h3 class="heading">Dễ Dàng Order</h3>
+            <p>
+              <b>Order nhanh chóng và tiện lợi</b>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 ftco-animate">
+        <div class="media d-block text-center block-6 services">
+          <div class="icon d-flex justify-content-center align-items-center mb-5">
+            <span class="flaticon-delivery-truck"></span>
+          </div>
+          <div class="media-body">
+            <h3 class="heading">Giao Hàng Nhanh</h3>
+            <p><b>
+                Giao hàng ngay sau khi order.</b></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 ftco-animate">
+        <div class="media d-block text-center block-6 services">
+          <div class="icon d-flex justify-content-center align-items-center mb-5">
+            <span class="flaticon-coffee-bean"></span>
+          </div>
+          <div class="media-body">
+            <h3 class="heading">Cà Phê Chất Lượng</h3>
+            <p>
+              <b>Cà phê chất lượng, thơm ngon khó cưỡng.</b>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
 
 <section class="ftco-section">
-	<div class="container">
-		<div class="row align-items-center">
-			<div class="col-md-6 pr-md-5">
-				<div class="heading-section text-md-right ftco-animate">
-					<span class="subheading">Discover</span>
-					<h2 class="mb-4">Our Menu</h2>
-					<p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-					<p><a href="#" class="btn btn-primary btn-outline-primary px-4 py-3">View Full Menu</a></p>
-				</div>
-			</div>
-			<div class="col-md-6">
-				<div class="row">
-					<div class="col-md-6">
-						<div class="menu-entry">
-							<a href="#" class="img" style="background-image: url(/view/images/menu-1.jpg);"></a>
-						</div>
-					</div>
-					<div class="col-md-6">
-						<div class="menu-entry mt-lg-4">
-							<a href="#" class="img" style="background-image: url(/view/images/menu-2.jpg);"></a>
-						</div>
-					</div>
-					<div class="col-md-6">
-						<div class="menu-entry">
-							<a href="#" class="img" style="background-image: url(/view/images/menu-3.jpg);"></a>
-						</div>
-					</div>
-					<div class="col-md-6">
-						<div class="menu-entry mt-lg-4">
-							<a href="#" class="img" style="background-image: url(/view/images/menu-4.jpg);"></a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-md-6 pr-md-5">
+        <div class="heading-section text-md-right ftco-animate">
+          <span class="subheading">Khám Phá</span><br>
+          <h2 class="mb-4">Menu</h2>
+          <p class="mb-4">
+            Menu của quán bao gồm nhiều loại đồ uống cà phê, trà, đồ ăn nhẹ,...
+            được pha chế từ nguyên liệu tươi ngon, chất lượng.
+            Chúng tôi luôn nỗ lực mang đến cho khách hàng những trải nghiệm ẩm thực tuyệt vời nhất.
+          </p>
+          <p>
+            <a href="#" class="btn btn-primary btn-outline-primary px-4 py-3">Xem Menu</a>
+          </p>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="menu-entry">
+              <a href="#" class="img" style="background-image: url(/view/images/menu-1.jpg)"></a>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="menu-entry mt-lg-4">
+              <a href="#" class="img" style="background-image: url(/view/images/menu-2.jpg)"></a>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="menu-entry">
+              <a href="#" class="img" style="background-image: url(/view/images/menu-3.jpg)"></a>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="menu-entry mt-lg-4">
+              <a href="#" class="img" style="background-image: url(/view/images/menu-4.jpg)"></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
 
-<section class="ftco-counter ftco-bg-dark img" id="section-counter" style="background-image: url(/view/images/bg_2.jpg);" data-stellar-background-ratio="0.5">
-	<div class="overlay"></div>
-	<div class="container">
-		<div class="row justify-content-center">
-			<div class="col-md-10">
-				<div class="row">
-					<div class="col-md-6 col-lg-3 d-flex justify-content-center counter-wrap ftco-animate">
-						<div class="block-18 text-center">
-							<div class="text">
-								<div class="icon"><span class="flaticon-coffee-cup"></span></div>
-								<strong class="number" data-number="100">0</strong>
-								<span>Coffee Branches</span>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-6 col-lg-3 d-flex justify-content-center counter-wrap ftco-animate">
-						<div class="block-18 text-center">
-							<div class="text">
-								<div class="icon"><span class="flaticon-coffee-cup"></span></div>
-								<strong class="number" data-number="85">0</strong>
-								<span>Number of Awards</span>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-6 col-lg-3 d-flex justify-content-center counter-wrap ftco-animate">
-						<div class="block-18 text-center">
-							<div class="text">
-								<div class="icon"><span class="flaticon-coffee-cup"></span></div>
-								<strong class="number" data-number="10567">0</strong>
-								<span>Happy Customer</span>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-6 col-lg-3 d-flex justify-content-center counter-wrap ftco-animate">
-						<div class="block-18 text-center">
-							<div class="text">
-								<div class="icon"><span class="flaticon-coffee-cup"></span></div>
-								<strong class="number" data-number="900">0</strong>
-								<span>Staff</span>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+<section class="ftco-counter ftco-bg-dark img" id="section-counter" style="background-image: url(/view/images/bg_2.jpg)" data-stellar-background-ratio="0.5">
+  <div class="overlay"></div>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-10">
+        <div class="row">
+          <div class="col-md-6 col-lg-3 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18 text-center">
+              <div class="text">
+                <div class="icon">
+                  <span class="flaticon-coffee-cup"></span>
+                </div>
+                <strong class="number" data-number="9">0</strong>
+                <span>Chi Nhánh</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18 text-center">
+              <div class="text">
+                <div class="icon">
+                  <span class="flaticon-coffee-cup"></span>
+                </div>
+                <strong class="number" data-number="3">0</strong>
+                <span>Giải Thưởng</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18 text-center">
+              <div class="text">
+                <div class="icon">
+                  <span class="flaticon-coffee-cup"></span>
+                </div>
+                <strong class="number" data-number="10567">0</strong>
+                <span>Khách Hàng Hài Lòng</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18 text-center">
+              <div class="text">
+                <div class="icon">
+                  <span class="flaticon-coffee-cup"></span>
+                </div>
+                <strong class="number" data-number="135">0</strong>
+                <span>Nhân Viên</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
