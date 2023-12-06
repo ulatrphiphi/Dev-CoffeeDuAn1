1bbe87477625dc0dfd1cd5d4960a9de3f43f9289
haohtpc05526 <trunghaopc23579@gmail.com>
2023-12-06 23:02:45 +0700

    [PHP] - Chỉnh sửa lại biểu đồ

--- admin/modules/thongke/bieudo.php
+++ admin/modules/thongke/bieudo.php
@@ -1,60 +1,61 @@
-<?php
-?>
-<div class="row">
-    <div class="col-12">
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Biểu Đồ</h3>
-            </div>
-            <!-- /.card-header -->
+<!DOCTYPE html>
+<html>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+
+<body>
+    <div class="card-footer">
+        <a href="index.php?act=thongke"><button type="submit" class="btn btn-primary" style="float:right;">Danh sách thống kê</button></a>
+    </div>
+    <canvas id="myChart" style="width:100%;max-width:1000px"></canvas>
+
+    <script>
+        const labels = [
             <?php
             $tongdm = count($listthongke);
             $i = 1;
             foreach ($listthongke as $thongke) {
                 extract($thongke);
                 if ($i == $tongdm) $dauphay = "";
+                else $dauphay = ",";
+                echo '"' . $thongke['tendm'] . '"' . $dauphay;
+                $i += 1;
             }
             ?>
+        ];
 
-            <div class="row">
-                <div id="piechart"></div>
-
-                <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-
-                <script type="text/javascript">
-                    // Load google charts
-                    google.charts.load('current', {
-                        'packages': ['corechart']
-                    });
-                    google.charts.setOnLoadCallback(drawChart);
-
-                    // Draw the chart and set the chart values
-                    function drawChart() {
-                        var data = google.visualization.arrayToDataTable([
-                            ['Danh mục', 'Số lượng sản phẩm'],
-                            <?php
-                            $tongdm = count($listthongke);
-                            $i = 1;
-                            foreach ($listthongke as $thongke) {
-                                extract($thongke);
-                                if ($i == $tongdm) $dauphay = "";
-                                else $dauphay = ",";
-                                echo "['" . $thongke['tendm'] . "'," . $thongke['countsp'] . "]" . $dauphay;
-                                $i += 1;
-                            }
-                            ?>
-                        ]);
-
-                        // Optional; add a title and set the width and height of the chart
-                        var options = {
-                            'title': 'THỐNG KÊ SẢN PHẨM THEO DANH MỤC',
-                            'width': 1100,
-                            'height': 800
-                        };
-
-                        // Display the chart inside the <div> element with id="piechart"
-                        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-                        chart.draw(data, options);
+        const data = {
+            labels: labels,
+            datasets: [{
+                backgroundColor: [
+                    "#b91d47", "#00aba9", "#2b5797", "#e8c3b9", "#1e7145",
+                    "#ff5733", "#33ff57", "#5733ff", "#ffa833", "#33ffa8"
+                ],
+                data: [
+                    <?php
+                    $i = 1;
+                    foreach ($listthongke as $thongke) {
+                        extract($thongke);
+                        if ($i == $tongdm) $dauphay = "";
+                        else $dauphay = ",";
+                        echo $thongke['countsp'] . $dauphay;
+                        $i += 1;
                     }
-                </script>
-            </div>
+                    ?>
+                ],
+            }]
+        };
+
+        new Chart("myChart", {
+            type: "pie",
+            data: data,
+            options: {
+                title: {
+                    display: true,
+                    text: "THỐNG KÊ SẢN PHẨM THEO DANH MỤC"
+                }
+            }
+        });
+    </script>
+</body>
+
+</html>
